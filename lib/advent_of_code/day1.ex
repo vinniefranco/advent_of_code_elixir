c5cb1af7ee3856c084658f6def1a327e6d9434b4
Vincent Franco <vince@freshivore.net>
2015-12-01 13:29:53 -0800

    Adding Part 2

--- lib/advent_of_code/day1.ex
+++ lib/advent_of_code/day1.ex
@@ -1,5 +1,7 @@
 defmodule AdventOfCode.Day1 do
   @doc ~S"""
+    Part 1
+
     Figures out which floor Santa needs to visit based on given `str`
 
     `str` contains two chars, both have meaning:
@@ -40,6 +42,39 @@ defmodule AdventOfCode.Day1 do
     |> String.length
     |> with_negatives(str)
     |> compute_floor
+  end
+
+  @doc ~S"""
+    Part 2
+
+    Now, given the same instructions, find the position of the first character
+    that causes him to enter the basement (floor -1). The first character in
+    the instructions has position 1, the second character has position 2, and so on.
+
+    ## Examples
+
+       iex> AdventOfCode.Day1.position(")")
+       1
+
+       iex> AdventOfCode.Day1.position("()())")
+       5
+
+
+  """
+  def position(str) do
+    on_position(str, 0, 0)
+  end
+
+  defp on_position(_str, position, -1) do
+    position
+  end
+
+  defp on_position(<<"(", rest::binary>>, position, total) do
+    on_position(rest, position + 1, total + 1)
+  end
+
+  defp on_position(<<")", rest::binary>>, position, total) do
+    on_position(rest, position + 1, total - 1)
   end
 
   defp with_negatives(total_length, str) do
